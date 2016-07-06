4059731a5d46e0d879808c9fd3379376d9b36e15
flaviocirillo <flavio.cirillo@neclab.eu>
2016-07-06 11:06:24 +0200

    Added possibility to wipe auto also postgres tables

--- ConfMan_Runner/setup.sh
+++ ConfMan_Runner/setup.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# PLEASE SET THE FOLLOWING FILE PATH BEFORE TO RUN
-#Configuration files to setup
-
-confman_configini='/opt/NEConfMan/ConfMan_Runner/confman/configuration/config.ini'
-confman_configxml='/opt/NEConfMan/fiwareRelease/confmanconfig/configurationManager/config/config.xml'
-confman_configproperties='/opt/NEConfMan/fiwareRelease/confmanconfig/configurationManager/config/config.properties'
-confman_launcher="/opt/NEConfMan/ConfMan_Runner/startConfigurationManager.sh"
-confman_launcherasdaemon="/opt/NEConfMan/ConfMan_Runner/startConfigurationManager_as_daemon.sh"
-confman_loggerproperties="$confman_bundlesconfigurationlocation/services/org.ops4j.pax.logging.properties"
-
-
-confman_version="5.1.3"
+# Functions
+. confman_functions.sh
 
 AUTOSETUP='false'
 PROPAGATEAUTO='false'
@@ -33,121 +23,7 @@ esac
 shift # past argument or value
 done
 
-# Functions
-function setPropertyIntoXML {
-	if grep -q "<entry key=\"$1\">.*<\/entry>" "$3"; then
-		
-		sed -i "s/<entry key=\"$1\">.*<\/entry>/<entry key=\"$1\">$2<\/entry>/g" "$3"
 
-	else
-		
-		sed -i "s/<\/properties>/<entry key=\"$1\">$2<\/entry>\n<\/properties>/g" "$3"		
-	fi
-}
-
-function setPropertyIntoIni {
-	key=$1
-	key=${key//\./\\\.}
-	key=${key//\//\\/}
-
-
-	if grep -q "$key=.*" "$3"; then
-		
-		sed -i "s/$key=.*/$key=$2/g" "$3"
-
-	else
-		
-		sed -i "s/# End of configurations/$key=$2\n# End of configurations/g" "$3"		
-	fi
-}
-
-function setPropertyIntoProperties {
-	key=$1
-	key=${key//\./\\\.}
-	key=${key//\//\\/}
-
-	if grep -q "$key=.*" "$3"; then
-		
-		sed -i "s/$key=.*/$key=$2/g" "$3"
-
-	else
-		value=${2//\\/}
-		echo "$key=$2" >> "$3"
-	fi
-}
-
-function setConfiguration {
-	
-	key=$1
-	key=${key//\./\\\.}
-	key=${key//\//\\/}
-	
-	value=$2
-	value=${value//\./\\\.}
-	value=${value//\//\\/}
-
-	
-	if grep -q "$key=.*" "$3"; then
-		sed -i "s/$key=.*/$key=\'$value\'/g" "$3"
-	else 
-		echo "$1='$2'" >> "$3"
-	fi
-
-}
-
-function disableBundle {
-
-	sed -i "s/.*$1-$confman_version.jar.*//g" $confman_configini
-
-}
-
-function enableBundle {
-	
-	found=`grep $1-$confman_version.jar confman/configuration/config.ini`
-	if [ -n "$found" ];
-	then
-		return
-	fi
-	
-	builder_target=`dirname $confman_configini`
-	builder_target=`dirname $builder_target`
-	builder_target=`dirname $builder_target`
-	builder_target=`dirname $builder_target`
-	builder_target="$builder_target/eu.neclab.iotplatform.confman.builder/target/builder-$confman_version-assembly/bundle/"
-		
-	if [ ! -e $builder_target/$1-$confman_version.jar ];
-	then
-		echo "WARNING bundle not found: $builder_target/$1-$confman_version.jar"
-		return;
-	fi
-	
-	
-	lastline=`awk '/./{line=$0} END{print line}' $confman_configini`
-	lastline=${lastline//\./\\\.}
-	lastline=${lastline//\//\\/}
-	if [[ $lastline != *,* ]] ; then
-		sed -i "s/$lastline/$lastline, \\\/g" $confman_configini
-	fi
-	
-	if [ "$2" == "nostart" ];
-	then
-		echo "../../eu.neclab.iotplatform.confman.builder/target/builder-$confman_version-assembly/bundle/$1-$confman_version.jar, \\" >> $confman_configini
-	else
-		echo "../../eu.neclab.iotplatform.confman.builder/target/builder-$confman_version-assembly/bundle/$1-$confman_version.jar@start, \\" >> $confman_configini
-	fi
-}
-
-function correctConfigIni {
-
-	lastline=`awk '/./{line=$0} END{print line}' $confman_configini`
-	correctlastline=${lastline/, \\/}
-	correctlastline=${correctlastline//\./\\\.}
-	correctlastline=${correctlastline//\//\\/}
-	sed -i "s/$correctlastline, \\\/$correctlastline/g" $confman_configini
-	
-	sed -i '/^$/d' $confman_configini
-	
-}
 
 # Escape characters
 sed -e 's/[]\/$*.^|[]/\\&/g' ./confman.conf.default > ./.confman.conf.default.escaped
@@ -204,16 +80,16 @@ then
 		setConfiguration "confman_bundlesconfigurationlocation" "$confman_bundlesconfigurationlocation" "confman.conf.local"
 		setConfiguration "confman_logfile" "$confman_logfile" "confman.conf.local"
 		
-		setConfiguration "confman_configini" "$confman_configini_auto" "setup.sh"
-		setConfiguration "confman_configxml" "$confman_configxml_auto" "setup.sh"
-		setConfiguration "confman_configproperties" "$confman_configproperties_auto" "setup.sh"
-		setConfiguration "confman_launcher" "$confman_launcher_auto" "setup.sh"
-		setConfiguration "confman_launcherasdaemon" "$confman_launcherasdaemon_auto" "setup.sh"
-		setConfiguration "confman_loggerproperties" "$confman_loggerproperties_auto" "setup.sh"
+		setConfiguration "confman_configini" "$confman_configini_auto" "confman_functions.sh"
+		setConfiguration "confman_configxml" "$confman_configxml_auto" "confman_functions.sh"
+		setConfiguration "confman_configproperties" "$confman_configproperties_auto" "confman_functions.sh"
+		setConfiguration "confman_launcher" "$confman_launcher_auto" "confman_functions.sh"
+		setConfiguration "confman_launcherasdaemon" "$confman_launcherasdaemon_auto" "confman_functions.sh"
+		setConfiguration "confman_loggerproperties" "$confman_loggerproperties_auto" "confman_functions.sh"
 		
 		if [ -n "$confman_version_auto" ];
 		then
-				setConfiguration "confman_version" "$confman_version_auto" "setup.sh"
+				setConfiguration "confman_version" "$confman_version_auto" "confman_functions.sh"
 		fi
 	fi
 
@@ -270,27 +146,5 @@ enableBundle restcontroller
 enableBundle serverconf nostart
 enableBundle utilitystorage
 enableBundle extensionmanager
-
-##ENABLE GEO DISCOVERY BUNDLES
-if [ "$confman_geodiscovery" == "enabled" ]
-then
-	enableBundle postgis
-	enableBundle geoindexer
-	enableBundle ngsi9geoextension
-else
-	disableBundle postgis
-	disableBundle geoindexer
-	disableBundle ngsi9geoextensionfi
-fi
-
-##ENABLE NGSI FEDERATION
-if [ "$iotbroker_historicalagent" == "enabled" ]
-then
-	enableBundle ngsihierarchyextension
-	enableBundle ngsihierarchystorage
-else
-	disableBundle ngsihierarchyextension
-	disableBundle ngsihierarchystorage
-fi
 
 correctConfigIni
